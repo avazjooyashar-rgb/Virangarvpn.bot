1d2322cf065e6951360825520720b3ea9da83f46
avazjooyashar-rgb <avazjooyashar@gmail.com>
2026-09-10 00:50:44 +0330

    Define SUPER_ADMIN_ID and update ADMIN_IDS
    Add SUPER_ADMIN_ID and include it in ADMIN_IDS.

--- config.py
+++ config.py
@@ -2,13 +2,14 @@ import os
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
+# 👑 Super Admin
+SUPER_ADMIN_ID = 691651240
 
+# 👥 Admins
 ADMIN_IDS = {
     int(user_id.strip())
     for user_id in os.getenv(
@@ -17,3 +18,6 @@ ADMIN_IDS = {
     ).split(",")
     if user_id.strip().isdigit()
 }
+
+# Super Admin همیشه ادمین است
+ADMIN_IDS.add(SUPER_ADMIN_ID)
